e292717701fbe11af3e0e7aab4808ac05e2bf43d
Dave Lafferty <c.david.lafferty@gmail.com>
2020-05-05 09:53:14 -0400

    Pinned requirements and made environment moreore configurable.

--- mm_tools/web/settings.py
+++ mm_tools/web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import distutils.util
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -20,16 +21,11 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    '8nycj64vk99yb4aeoi)47)r5(6%05n&m0mk30g)1br5ayu*7-e'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = bool(distutils.util.strtobool(os.environ.get("DEBUG", "False")))
 ALLOWED_HOSTS = ['*',]
-
 
 # Application definition
 
